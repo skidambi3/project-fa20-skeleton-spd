d190a7e52c856ffffc5fa1dda91cfa7974f71ae0
Saisaran Kidambi <skidambi@berkeley.edu>
2020-11-23 21:48:56 -0800

    input

---
+++ create_input.py
@@ -0,0 +1,20 @@
+from parse import *
+import matplotlib.pyplot as plt
+import re
+import os
+import random
+
+import networkx as nx
+
+import utils
+
+G = nx.Graph()
+for i in range(10):
+	G.add_node(i)
+	for j in range(10):
+		if i != j:
+			G.add_node(j)
+			G.add_edge(i, j)
+stress_budget = 80.5
+path = "./20.in"
+write_input_file(G, stress_budget, path)
