efa8b9871e1d1958b020a48f43e31b47908fcd2d
Saisaran Kidambi <skidambi@berkeley.edu>
2020-12-07 21:17:11 -0800

    sub1

--- solver.py
+++ solver.py
@@ -294,7 +294,7 @@ def solve(G, s):
     #     if (u, v) == (-1, -1):
     #         break
     #     put(u, v)
-    print(rooms)
+    # print(rooms)
     return (convert_dictionary(rooms), len(rooms))
 
 
@@ -303,23 +303,23 @@ def solve(G, s):
 
 # Usage: python3 solver.py test.in
 
-if __name__ == '__main__':
-     assert len(sys.argv) == 2
-     path = sys.argv[1]
-     G, s = read_input_file(path)
-     D, k = solve(G, s)
-     assert is_valid_solution(D, G, s, k)
-     print("Total Happiness: {}".format(calculate_happiness(D, G)))
-     write_output_file(D, 'outputs/small-1.out')
+# if __name__ == '__main__':
+#      assert len(sys.argv) == 2
+#      path = sys.argv[1]
+#      G, s = read_input_file(path)
+#      D, k = solve(G, s)
+#      assert is_valid_solution(D, G, s, k)
+#      print("Total Happiness: {}".format(calculate_happiness(D, G)))
+#      write_output_file(D, 'outputs/small-1.out')
 
 
 # For testing a folder of inputs to create a folder of outputs, you can use glob (need to import it)
-#if __name__ == '__main__':
-#     inputs = glob.glob('inputs/*')
-#     for input_path in inputs:
-#         output_path = 'outputs/' + basename(normpath(input_path))[:-3] + '.out'
-#         G, s = read_input_file(input_path)
-#         D, k = solve(G, s)
-#         assert is_valid_solution(D, G, s, k)
-#         happiness = calculate_happiness(D, G)
-#         write_output_file(D, output_path)
+if __name__ == '__main__':
+    inputs = glob.glob('inputs/*')
+    for input_path in inputs:
+        output_path = 'outputs/' + basename(normpath(input_path))[:-3] + '.out'
+        G, s = read_input_file(input_path)
+        D, k = solve(G, s)
+        assert is_valid_solution(D, G, s, k)
+        happiness = calculate_happiness(D, G)
+        write_output_file(D, output_path)
